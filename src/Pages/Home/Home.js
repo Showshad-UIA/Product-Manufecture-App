3d2f31e9d1f2e2cfb08fcf7fd81fae44e30fab2c
raihan <raihanulmcse@mail.com>
2022-05-25 04:44:37 +0800

    log in added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Footer } from "react-day-picker";
 
 const Home = () => {
 	return (
--- src/Pages/Shared/NavBar.js
+++ src/Pages/Shared/NavBar.js
@@ -14,6 +14,34 @@ const NavBar = () => {
 			<li>
 				<Link to="/order">Order</Link>
 			</li>
+			<li>
+				<Link to="/blog">Blog</Link>
+			</li>
+			<li>
+				<Link to="/portfolio">Portfolio</Link>
+			</li>
+			<li>
+				<Link to="/dashboard">
+					Dashboard
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						width="20"
+						height="20"
+						viewBox="0 0 24 24"
+					>
+						<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+					</svg>
+				</Link>
+				<ul class="p-2 bg-base-100">
+					<li>
+						<Link to="/review">Review</Link>
+					</li>
+					<li>
+						<Link to="/items">Items</Link>
+					</li>
+				</ul>
+			</li>
 
 			<li>
 				<Link to="/login">Login</Link>
